cbd7931016b7fd41643834d3a16f4b8183860dd0
omerhan.cakmak <omerhan.cakmak2@gamil.com>
2022-04-17 03:39:37 +0300

    main.py was updatet from test

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import utime
 import hmc5883
 
 led = machine.Pin(25,machine.Pin.OUT)
-i2c_conn = machine.I2C(1,scl=machine.Pin(15),sda=machine.Pin(14))
+#i2c_conn = machine.I2C(id=1,scl=machine.Pin(15),sda=machine.Pin(14))
 
-devices = i2c_conn.scan()
+#devices = i2c_conn.scan()
 
-if devices:
-    for port in devices:
-        print(hex(port))
+#if devices:
+#    for port in devices:
+#        print(hex(port))
         
 
-dev = hmc5883.HMC5883L
+dev = hmc5883.HMC5883L()
+
 
 
 
